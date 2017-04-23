54fd95dec7de9a08eb06482f3d8f141bb5fa4dbf
Nils Schweinsberg <mail@nils.cc>
2017-04-23 18:58:58 +0200

    Define content texts

--- src-hs/LiftMe/Html/Content.hs
+++ src-hs/LiftMe/Html/Content.hs
@@ -5,6 +5,8 @@ module LiftMe.Html.Content where
 import Control.Monad
 import Control.Monad.Trans
 
+import Data.Text (Text)
+
 import Happstack.Server
 import Text.Blaze.Html5 (Html, (!))
 import qualified Text.Blaze.Html5 as H
@@ -12,6 +14,18 @@ import qualified Text.Blaze.Html5.Attributes as A
 
 import LiftMe.Database
 
+-- | All texts used on content page
+data ContentText = ContentText
+  { m_ctExercises :: Text
+  }
+
+-- | Default translation of content texts
+defaultContentText :: ContentText
+defaultContentText = ContentText
+  { m_ctExercises = "Exercises"
+  }
+
+-- | Get current content HTML
 getContent :: DB -> ServerPart Html
 getContent db = do
 
@@ -22,8 +36,13 @@ getContent db = do
       exs <- exercises db cat
       return (cat, exs)
 
+  -- current text translation
+  let contentText = defaultContentText
+
+  -- build HTML
   return $ do
-    H.h1 "Exercises"
+
+    H.h1 $ H.toHtml (m_ctExercises contentText)
 
     forM_ exs $ \(cat, exs) -> do
       H.h2 $ H.toHtml (m_categoryName cat)
