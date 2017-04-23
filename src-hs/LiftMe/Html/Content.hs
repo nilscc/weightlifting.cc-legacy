af41f6b49fb4917fb6d8cd375b65c7fff9dbdc70
Nils Schweinsberg <mail@nils.cc>
2017-04-23 18:52:26 +0200

    Show list of BVDG exercises on front page

--- src-hs/LiftMe/Html/Content.hs
+++ src-hs/LiftMe/Html/Content.hs
@@ -2,14 +2,32 @@
 
 module LiftMe.Html.Content where
 
-import LiftMe.Database
+import Control.Monad
+import Control.Monad.Trans
 
 import Happstack.Server
-import Text.Blaze.Html5 (Html)
+import Text.Blaze.Html5 (Html, (!))
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 
+import LiftMe.Database
+
 getContent :: DB -> ServerPart Html
-getContent db = return $ do
-  H.h1 "Main content."
-  H.p "bla bla bla"
+getContent db = do
+
+  -- get list of exercises from database
+  exs <- liftIO $ do
+    cats <- categories db
+    forM cats $ \cat -> do
+      exs <- exercises db cat
+      return (cat, exs)
+
+  return $ do
+    H.h1 "Exercises"
+
+    forM_ exs $ \(cat, exs) -> do
+      H.h2 $ H.toHtml (m_categoryName cat)
+      H.ul ! A.id (H.toValue $ "category_" ++ show (m_categoryId cat)) $ do
+        forM_ exs $ \ex -> do
+          H.li ! A.id (H.toValue $ "exercise_" ++ show (m_exerciseId ex)) $
+            H.toHtml (m_exerciseName ex)
